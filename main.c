90a0082f2535321aa37b4c664f23bc8ba2deb165
ihsan314 <43079005+ihsan314@users.noreply.github.com>
2018-09-07 15:20:38 -0700

    Update main.c
    It is mostly done. All that is missing is the implementation of DCONST and discarding the value of a in the implementation of PRINT. There is test code in main to test the code I added.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : Arithmetic Machine
- Author      : UBC Launchpad
+ Author      : UBC Launchpad (edited by Ihsan Olawale)
  Version     : 1.0
  Copyright   :
  Description : Stack-based virtual machine that performs simple arithmetic.
@@ -76,20 +76,25 @@ int run(VM* vm){
         case HALT: return EXIT_SUCCESS;  // exit successfully
         case NOP: break;    // pass
         case DCONST_M1:     // push -1.0 onto stack
+            PUSH(vm, -1.0);
             // TODO: implement this.
             break;
         case DCONST_0:      // push 0.0 onto stack
+            PUSH(vm, 0.0);
             // TODO: implement this.
             break;
         case DCONST_1:      // push 1.0 onto stack
+            PUSH(vm, 1.0);
             // TODO: implement this.
             break;
         case DCONST_2:      // push 2.0 onto stack
+            PUSH(vm, 2.0);
             // TODO: implement this.
             break;
         case DCONST:        // reads next 8 bytes of opcode as a double, and stores it on the stack.
             // TODO: implement this.
             // HINT: use memcpy to read next 8 bytes of code as a double. make sure you consider endianness.
+            /* I am not too sure about this one; I did not get to memcpy when reading up on C. */
             break;
         case ADD:           // add two doubles from top of stack and push result back onto stack
             b = POP(vm);
@@ -98,33 +103,53 @@ int run(VM* vm){
             break;
         case MUL:           // multiply two doubles from top of stack and push result back onto stack
             // TODO: implement this.
+            b = POP(vm);
+            a = POP(vm);
+            PUSH(vm, a * b);
             break;
         case SUB:           // subtract two doubles from top of stack and push result back onto stack
             // TODO: implement this.
+            b = POP(vm);
+            a = POP(vm);
+            PUSH(vm, a - b);
             break;
         case DIV:          // divide two doubles from top of stack and push result back onto stack
             //TODO: implement this.
             // HINT: make sure to deal with the division by zero case.
+            b = POP(vm);
+            if (b == 0)
+                printf ("Cannot divide by 0\n");
+            else {
+                a = POP(vm);
+                PUSH(vm, a / b);
+            }
             break;
         case NEG:                         // negates top of stack
             //TODO: implement this.
+            PUSH(vm, POP(vm) * -1);
             break;
         case LD1:          // put value from r1 on top of stack
             // TODO: implement this.
+            PUSH(vm, vm->r1);
             break;
         case ST1:                         // store top of stack in r1
             // TODO: implement this.
+            r1 = POP(vm);
             break;
         case LD2:           // put value from r2 on top of stack
             // TODO: implement this.
             // HINT: should be similar to LD1.
+            PUSH(vm, vm->r1);
             break;
         case ST2:                         // store top of stack in r2
             // TODO: implement this.
             // HINT: should be similar to ST1.
+            r2 = POP(vm);
             break;
         case PRINT:                       // print top of stack, (and discard value afterwards.)
             // TODO: implement this.
+            a = POP(vm);
+            printf ("%g\n", a);
             break;
         default:
             printf("InvalidOpcodeError: %x\n", opcode);  // terminate program at unknown opcode and show error.
@@ -139,11 +164,41 @@ int main(void) {
 	/* in a real VM, we'd read bytecode from a file, but for brevity's sake we'll read
 	from an array.
 	*/
-	// simple example: push 2 onto stack, push 1 onto stack, subtract them, print the result, exit (should print 1.0)
+	/* simple example: push 2 onto stack, push 1 onto stack, subtract them, print the result (should print 1.0)
+       then push 2 onto stack, push 2 onto stack, multiply them, print the result (should print 4.0)
+       then push 0 onto stack, and try dividing (should print "Cannot divide by zero")
+       then negate top of stack, print the result (should print -2.0)
+       then divide and print the result (shoudl print 1.0)
+       then multiply
+       then store top of stack in r1 and print the new top of stack (should print 2.0)
+       then store top of stack in r2 and print the new top of stack (should print 2.0)
+       then push r1 on top of stack and print the result (should print 4.0)
+       then push r2 on top of stack and print the result (should print 2.0)
+       then exit
+    */
 	char bytecode[] = { DCONST_2,
 		DCONST_1,
 		SUB,
 		PRINT,
+        DCONST_2,
+        DCONST_2,
+        MUL,
+        PRINT,
+        DCONST_0,
+        DIV,
+        NEG,
+        PRINT,
+        DIV,
+        PRINT,
+        MUL,
+        ST1,
+        PRINT,
+        ST2,
+        PRINT,
+        LD1,
+        PRINT,
+        LD2,
+        PRINT,
 		HALT };
 	VM* vm = newVM(bytecode /* program to execute */ );
 	int exit_status = run(vm);
